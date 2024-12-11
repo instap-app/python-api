c342823597dac12cae193acc6231ca9e96d19de4
Pawel <rzepaw@gmail.com>
2024-12-11 12:59:35 +0100

    Seems to work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="instapi",
-    version="0.3.5",
+    version="0.3.6",
     author="Instap",
     author_email="api@instap.app",
     description="Instap API for Python",
